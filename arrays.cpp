ede880bf2b58dbbb590f643ae3b4b18b21cea2a8
gmann473 <jesuisgxry@gmail.com>
2025-09-04 16:13:16 -0400

    Create arrays.cpp
    Simple code which demonstrates the use of arrays.

---
+++ arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+void traverse(int arr[], int size){
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+cout <<endl;
+}
+int main() {
+    int capacity = 10;
+    int arr[capacity]={1,2,3,4,5};
+    int size = 5;
+    
+    traverse(arr, size);
+    
+    return 0;
+}
+
+
+ 
